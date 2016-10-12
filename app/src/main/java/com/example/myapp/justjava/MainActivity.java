3aa7ecbfe514b1a3a265d3fce368d72c21fad79e
mmanohara94 <elemanasi@gmail.com>
2016-10-12 14:15:37 +0530

    Pop up to show availibility

--- app/src/main/java/com/example/myapp/justjava/MainActivity.java
+++ app/src/main/java/com/example/myapp/justjava/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.myapp.justjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +22,16 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.listView);
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, food);
-
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String foods = (String)adapterView.getItemAtPosition(i);
+                 Toast.makeText(view.getContext(),""+foods+ " is available", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
