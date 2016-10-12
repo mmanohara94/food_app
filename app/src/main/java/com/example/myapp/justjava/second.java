bca7f864f6caec2963c30f272de88bf5f6ecab53
mmanohara94 <elemanasi@gmail.com>
2016-10-12 17:49:52 +0530

    increase decrease quantity buttons

--- app/src/main/java/com/example/myapp/justjava/second.java
+++ app/src/main/java/com/example/myapp/justjava/second.java
@@ -2,12 +2,52 @@ package com.example.myapp.justjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class second extends AppCompatActivity {
+    private Button _decrease;
+    private Button _increase;
+    private TextView _value;
+    private static int _counter = 0;
+    private String _stringVal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        _decrease = (Button) findViewById(R.id.decrease);
+        _increase = (Button) findViewById(R.id.increase);
+        _value = (TextView) findViewById(R.id.quantity_text_view);
+
+        _decrease.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Log.d("src", "Decreasing value...");
+                if (_counter>0)
+                _counter--;
+                _stringVal = Integer.toString(_counter);
+                _value.setText(_stringVal);
+    }
+
+ });
+        _increase.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Log.d("src", "Increasing value...");
+                _counter++;
+                _stringVal = Integer.toString(_counter);
+                _value.setText(_stringVal);
+            }
+        });
+
     }
 }
